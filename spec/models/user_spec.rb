b7b37c66a1c55792b4e66682f81d6170106c19ae
efgalvao <solfieri@hotmail.com>
2024-01-14 12:11:47 -0300

    adjust user specs to use shoulda_matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,24 +2,22 @@ require 'rails_helper'
 
 RSpec.describe User do
   describe 'validations' do
-    it 'is valid with valid attributes' do
-      expect(described_class.new(name: 'Name', username: 'Username', email: 'e@mail.com', password: 'password')).to be_valid
-    end
+    subject(:new_user) { described_class.new(attributes_for(:user)) }
 
     it 'is not valid without a name' do
-      expect(described_class.new(username: 'Username', email: 'e@mail.com', password: 'password')).not_to be_valid
+      expect(new_user).to validate_presence_of(:name)
     end
 
     it 'is not valid without a username' do
-      expect(described_class.new(name: 'Name', email: 'e@mail.com', password: 'password')).not_to be_valid
+      expect(new_user).to validate_presence_of(:username)
     end
 
     it 'is not valid without a email' do
-      expect(described_class.new(name: 'Name', username: 'Username', password: 'password')).not_to be_valid
+      expect(new_user).to validate_presence_of(:email)
     end
 
     it 'is not valid without a password' do
-      expect(described_class.new(name: 'Name', username: 'Username', email: 'e@mail.com')).not_to be_valid
+      expect(new_user).to validate_presence_of(:password)
     end
   end
 end
