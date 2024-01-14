6fbff3a6a1acc78d362f2d08ea558cbe529d871c
efgalvao <solfieri@hotmail.com>
2024-01-14 09:01:31 -0300

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+        devise_for :users, controllers: {
+          registrations: 'users/registrations'      }
   devise_scope :user do
     get 'sign_in', to: 'devise/sessions#new'
-    get 'sign_up', to: 'devise/registrations#new'
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
