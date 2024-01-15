bfefe8c3d1399b0aa0a83b0387f3efa566e2fdf4
efgalvao <solfieri@hotmail.com>
2024-01-15 08:52:34 -0300

    add financing routes and controller

---
+++ app/controllers/financing/financings_controller.rb
@@ -0,0 +1,60 @@
+module Financing
+  class FinancingsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :financing, only: %i[show destroy edit update]
+
+    def index
+      @financings = Financings::Financing.where(user_id: current_user.id).all
+    end
+
+    def show
+      serialized_financing = FinancingSerializer.new(@financing).serializable_hash[:data][:attributes]
+
+      render json: serialized_financing, status: :ok
+    end
+
+    def new
+      @financing = Financings::Financing.new
+    end
+
+    def edit; end
+
+    def create
+      @financing = FinancingServices::CreateFinancing.call(financing_params)
+      if @financing.valid?
+        respond_to do |format|
+          format.html { redirect_to financings_path, notice: 'Financiamento criado com sucesso.' }
+          format.turbo_stream
+        end
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      if @financing.update(financing_params)
+        redirect_to financings_path, notice: 'Financiamento atualizado com sucesso.'
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @financing.destroy
+      render json: { status: ' ok' }, status: :ok
+    end
+
+    private
+
+    def financing
+      @financing ||= Financings::Financing
+                     .find_by(id: params[:id], user_id: current_user.id)
+    end
+
+    def financing_params
+      params.require(:financing)
+            .permit(:name, :borrowed_value_cents, :installments)
+            .merge(user_id: current_user.id)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,15 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'sign_in', to: 'devise/sessions#new'
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root 'home#index'
 
   resources :categories
+
+  scope module: 'financing' do
+    resources :financings do
+      resources :installments, only: %i[index new create]
+    end
+  end
 end
