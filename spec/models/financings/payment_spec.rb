654b6fb5c4725075617c997dec6d374e648d8d72
efgalvao <solfieri@hotmail.com>
2024-01-29 19:40:34 -0300

    adjust spec

--- spec/models/financings/payment_spec.rb
+++ spec/models/financings/payment_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Financings::Payment do
-  subject(:installment) { build(:payment) }
+  subject(:payment) { build(:payment) }
 
   it 'belongs to a financing' do
     expect(payment).to belong_to(:financing)
