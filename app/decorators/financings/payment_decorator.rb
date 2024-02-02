62edd72ee198da75df0ce298838432bfbd408440
efgalvao <solfieri@hotmail.com>
2024-02-02 09:08:53 -0300

    refactor payment decorator

--- app/decorators/financings/payment_decorator.rb
+++ app/decorators/financings/payment_decorator.rb
@@ -5,38 +5,43 @@ module Financings
     end
 
     def parcel_value
-      (object.amortization_cents +
+      value = (object.amortization_cents +
        object.interest_cents +
        object.insurance_cents +
        + object.fees_cents + object.adjustment_cents) / 100.0
+      format_currency(value)
     end
 
     def interest
-      object.interest_cents / 100.0
+      format_currency(object.interest_cents / 100.0)
     end
 
     def amortization
-      object.amortization_cents / 100.0
+      format_currency(object.amortization_cents / 100.0)
     end
 
     def insurance
-      object.insurance_cents / 100.0
+      format_currency(object.insurance_cents / 100.0)
     end
 
     def fees
-      object.fees_cents / 100.0
+      format_currency(object.fees_cents / 100.0)
     end
 
     def adjustment
-      object.adjustment_cents / 100.0
+      format_currency(object.adjustment_cents / 100.0)
     end
 
     def monetary_correction
-      object.monetary_correction_cents / 100.0
+      format_currency(object.monetary_correction_cents / 100.0)
     end
 
     def payment_date
-      object.payment_date&.strftime('%d/%m/%Y')
+      object.payment_date
+    end
+
+    def format_currency(value)
+      ActionController::Base.helpers.number_to_currency(value, unit: 'R$ ', separator: ',', delimiter: '.')
     end
 
     delegate :id, :parcel, :paid_parcels, :new_record?, :errors, :ordinary, :persisted?, :valid?,
