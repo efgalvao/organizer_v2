b99e3e9262f5a86c68d0e2c9c6aab29f1db19bf5
efgalvao <solfieri@hotmail.com>
2024-02-11 07:24:12 -0300

    refactor update_account service

--- app/services/account_services/update_account.rb
+++ app/services/account_services/update_account.rb
@@ -1,12 +1,22 @@
 module AccountServices
   class UpdateAccount
+    def initialize(account_params)
+      @account_params = account_params
+    end
+
     def self.update(account_params)
-      update_account(account_params)
+      new(account_params).update
+    end
+
+    def update
+      update_account
     end
 
     private
 
-    def self.update_account(account_params)
+    attr_reader :account_params
+
+    def update_account
       account = Account::Account.find(account_params[:id])
       account.update(account_params)
       account
