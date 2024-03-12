8f181fdd0eb9d240ad76de0395e940a7c34136fb
efgalvao <solfieri@hotmail.com>
2024-03-12 07:39:25 -0300

    Add UpdateAccountBalance service and spec

---
+++ app/services/account_services/update_account_balance.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module AccountServices
+  class UpdateAccountBalance
+    def initialize(params)
+      @params = params
+    end
+
+    def self.call(params)
+      new(params).call
+    end
+
+    def call
+      update_balance
+    end
+
+    private
+
+    attr_reader :params
+
+    def account
+      @account ||= Account::Account.find(params[:account_id])
+    end
+
+    def update_balance
+      account.update(update_params)
+      account
+    end
+
+    def update_params
+      {
+        balance_cents: new_balance
+      }
+    end
+
+    def new_balance
+      account.balance_cents + params[:value_cents]
+    end
+  end
+end
