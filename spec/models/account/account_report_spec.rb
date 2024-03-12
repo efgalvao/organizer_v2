c45e905fd3a0ba79e088438c81c94f272ceffd4c
efgalvao <solfieri@hotmail.com>
2024-03-12 07:38:43 -0300

    Add month_report method to Account::AccountReport model

--- spec/models/account/account_report_spec.rb
+++ spec/models/account/account_report_spec.rb
@@ -12,4 +12,18 @@ RSpec.describe Account::AccountReport do
     it { is_expected.to validate_presence_of(:reference) }
     it { is_expected.to validate_uniqueness_of(:reference).scoped_to(:account_id) }
   end
+
+  describe '#month_report' do
+    let(:account) { create(:account) }
+    let!(:past_month_report) do
+      create(:account_report, account_id: account.id,
+                              reference: Time.zone.now.last_month.strftime('%m%y'))
+    end
+
+    it 'returns the month account report' do
+      expect(described_class.month_report(account_id: account.id,
+                                          reference_date: Time.zone.now.last_month))
+        .to eq(past_month_report)
+    end
+  end
 end
