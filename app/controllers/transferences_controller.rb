96087229a0a8a7896ba5ef980572d3685ca4af47
efgalvao <solfieri@hotmail.com>
2024-03-16 20:47:14 -0300

    Add TransferencesController with index, new, and create actions

---
+++ app/controllers/transferences_controller.rb
@@ -0,0 +1,30 @@
+class TransferencesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @transferences = TransferenceServices::FetchTransferences.call(current_user.id).decorate
+  end
+
+  def new
+    @transference = Transference.new
+  end
+
+  def create
+    @transference = TransferenceServices::BuildTransference.call(transference_params).decorate
+
+    if @transference.save
+      respond_to do |format|
+        format.html { redirect_to transferences_path, notice: 'Transferência criada.' }
+        format.turbo_stream
+      end
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def transference_params
+    params.require(:transference).permit(:sender_id, :receiver_id, :date, :value_cents).merge(user_id: current_user.id)
+  end
+end
