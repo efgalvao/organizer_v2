88375b8e06fa02a3571eacda8c9586c29cc1198e
efgalvao <solfieri@hotmail.com>
2024-06-07 17:27:11 -0300

    feat: Add new action to InvoicesController for creating invoices

---
+++ app/controllers/invoices_controller.rb
@@ -0,0 +1,30 @@
+class InvoicesController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @accounts = Account::Account.where(user_id: current_user.id).except_card_accounts
+    @card_id = params[:card_id]
+    respond_to do |format|
+      format.turbo_stream
+      format.html
+    end
+  end
+
+  def create
+    response = CreditServices::ProcessInvoicePayment.call(invoice_params)
+    if response == 'success'
+      respond_to do |format|
+        format.html { redirect_to card_path(id: params[:card_id]), notice: 'Pagamento efetuado.' }
+        format.turbo_stream { flash.now[:notice] = 'Pagamento efetuado.' }
+      end
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def invoice_params
+    params.require(:invoice).permit(:sender_id, :receiver_id, :value, :date)
+  end
+end
