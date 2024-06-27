b75249d7dedc711805eeccbdcb43010e959c533c
efgalvao <solfieri@hotmail.com>
2024-06-27 18:07:08 -0300

    feat: Add outstanding balance and outstanding parcels to financing show page
    This commit adds the display of outstanding balance and outstanding parcels to the financing show page. It introduces new methods `outstanding_balance` and `outstanding_parcels` in the `Financings::FinancingDecorator` to calculate and retrieve the values. The financing show view template is updated to include these new fields.
    
    These changes provide users with important information about the remaining balance and parcels for a financing, enhancing their understanding of the financing status.

--- app/decorators/financings/financing_decorator.rb
+++ app/decorators/financings/financing_decorator.rb
@@ -10,6 +10,37 @@ module Financings
       object.name.capitalize
     end
 
+    def outstanding_parcels
+      object.payments.reduce(object.installments) { |result, parcel| result - parcel.paid_parcels }
+    end
+
+    def outstanding_balance
+      value = object.payments.order(payment_date: :asc).reduce(object.borrowed_value_cents) do |result, parcel|
+        (result - parcel.amortization_cents) + parcel.monetary_correction_cents
+      end
+      value / 100.0
+    end
+
+    def total_amortization
+      object.payments.sum(:amortization_cents) / 100.0
+    end
+
+    def total_interest_paid
+      object.payments.sum(:interest_cents) / 100.0
+    end
+
+    def ordinary_parcels
+      object.payments.ordinary.count
+    end
+
+    def non_ordinary_parcels
+      object.payments.where(ordinary: false).count
+    end
+
+    def monetary_correction
+      object.payments.sum(:monetary_correction_cents) / 100.0
+    end
+
     delegate :id, :installments, to: :object
   end
 end
