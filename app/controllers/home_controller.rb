3653455247a9e679b99fed829a414af6a7579daf
Edmar Galvao <61836657+efgalvao@users.noreply.github.com>
2024-07-01 14:04:26 -0300

    feat: Refactor home controller to use structured data for report (#66)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,16 @@ class HomeController < ApplicationController
   end
 
   def show
-    # puts 'params ---->', params.inspect
-    # @user_report = current_user.current_month_report.decorate
-    @user_report = UserServices::ConsolidatedUserReport.new(current_user.id).call.decorate
-    @past_reports = UserServices::FetchUserReports.fetch_reports(current_user.id).decorate
+    report_data = Struct.new(:user_report, :past_reports, :past_reports_chart_data)
+
+    past_reports = UserServices::FetchUserReports.fetch_reports(current_user.id)
+
+    data = report_data.new(
+      UserServices::ConsolidatedUserReport.new(current_user.id).call.decorate,
+      past_reports.decorate,
+      UserServices::CreateUserSummaryChartData.call(reports: past_reports)
+    )
+
+    @report_data = data
   end
 end
