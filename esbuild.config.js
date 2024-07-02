8908dd483264922012eec6b8e5c5829f8c3270c9
efgalvao <solfieri@hotmail.com>
2024-07-02 18:44:30 -0300

    chore: Update esbuild version to 0.23.0 and optimize build script

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,15 +1,24 @@
-const esbuild = require('esbuild');
-const sassPlugin = require('esbuild-plugin-sass');
+// esbuild.config.js
 
-esbuild.build({
+const sassPlugin = require('esbuild-plugin-sass');
+const { readFile } = require('fs/promises');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = {
   entryPoints: ['app/javascript/application.js'],
   bundle: true,
-  sourcemap: true,
-  outdir: 'app/assets/builds',
+  minify: isProduction,
+  sourcemap: !isProduction,
+  outdir: 'public/assets/builds',
   publicPath: '/assets',
-  plugins: [sassPlugin()],
-  loader: {
-    '.js': 'jsx',
-    '.scss': 'css',
-  },
-}).catch(() => process.exit(1));
+  plugins: [
+    sassPlugin({
+      async resolve({ path }) {
+        if (path.endsWith('.scss')) {
+          return { loader: 'scss', contents: await readFile(path, 'utf8') };
+        }
+      },
+    }),
+  ],
+};
