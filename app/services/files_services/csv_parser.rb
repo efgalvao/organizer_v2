4ebe822b4a5d107439a056c49eaf77af27bb2b09
efgalvao <solfieri@hotmail.com>
2024-07-04 18:16:10 -0300

    feat: Add CSV file parser service and specs

---
+++ app/services/files_services/csv_parser.rb
@@ -0,0 +1,44 @@
+module FilesServices
+  class CsvParser
+    require 'csv'
+
+    def initialize(file)
+      @file = file
+    end
+
+    def self.call(file)
+      new(file).call
+    end
+
+    def call
+      parse_file
+    rescue NoMethodError
+      'Invalid CSV file'
+    end
+
+    private
+
+    attr_reader :file
+
+    def parse_file
+      content = { transactions: [],
+                  transferences: [],
+                  credit: [],
+                  invoices: [] }
+
+      CSV.foreach(Rails.root + file.path, headers: csv_headers) do |row|
+        content[:transactions] << row.to_h
+      end
+      content
+    end
+
+    def csv_headers
+      %i[ kind
+          account
+          date
+          value
+          title
+          category]
+    end
+  end
+end
