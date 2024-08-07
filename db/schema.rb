c2bce0c0df5e0fd46f23f99a8f225e79726f3ff9
efgalvao <solfieri@hotmail.com>
2024-08-07 20:51:06 -0300

    chore: Update borrowed_value_cents column to decimal in financings migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_30_021809) do
+ActiveRecord::Schema[7.0].define(version: 2024_08_07_202032) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,7 +61,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_30_021809) do
 
   create_table "financings", force: :cascade do |t|
     t.string "name", null: false
-    t.integer "borrowed_value_cents", default: 0, null: false
+    t.decimal "borrowed_value", precision: 15, scale: 2, default: "0.0", null: false
     t.integer "installments", default: 0, null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
