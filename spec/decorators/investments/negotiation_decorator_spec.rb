e197e23a6921ef4bf978c7884d9ef8c17f0f0126
efgalvao <solfieri@hotmail.com>
2024-08-09 14:23:34 -0300

    Refactor negotiation decorator to format amount as currency

--- spec/decorators/investments/negotiation_decorator_spec.rb
+++ spec/decorators/investments/negotiation_decorator_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.describe Investments::NegotiationDecorator do
   subject(:decorated_negotiation) { negotiation.decorate }
 
-  let(:negotiation) { create(:negotiation, date: '2024-03-16') }
+  let(:negotiation) { create(:negotiation, date: '2024-03-16', amount: 1.23) }
   let(:investment) { create(:investment, negotiations: [negotiation]) }
   let(:account) { create(:account, investments: [investment]) }
 
   describe '#amount' do
     it 'returns the negotiated amount' do
-      expect(decorated_negotiation.amount).to eq(negotiation.amount_cents / 100.0)
+      expect(decorated_negotiation.amount).to eq('R$ 1,23')
     end
   end
 
