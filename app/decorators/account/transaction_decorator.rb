e343a6612ac861b078ac932887e8b7eb68d21fb7
efgalvao <solfieri@hotmail.com>
2024-08-09 11:16:51 -0300

    chore: Update transaction decorator to use "amount" instead of "value"

--- app/decorators/account/transaction_decorator.rb
+++ app/decorators/account/transaction_decorator.rb
@@ -2,8 +2,8 @@ module Account
   class TransactionDecorator < Draper::Decorator
     delegate_all
 
-    def value
-      object.value_cents / 100.0
+    def amount
+      ActionController::Base.helpers.number_to_currency(object.amount, unit: 'R$ ', separator: ',', delimiter: '.')
     end
 
     def date
