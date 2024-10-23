75e318a49f509f15ece5debde355f7f1b0bd1d1d
efgalvao <solfieri@hotmail.com>
2024-10-23 13:49:00 -0300

    Refactor home controller to use current_user_id instead of current_user.id

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,18 +6,18 @@ class HomeController < ApplicationController
   end
 
   def show
-    report_data = Struct.new(:user_report, :past_reports, :past_reports_chart_data, :accounts, :cards, :expense_by_category)
+    report_data = Struct.new(:user_report, :past_reports, :past_reports_chart_data, :accounts, :cards,
+                             :expense_by_category)
 
-    past_reports = UserServices::FetchUserReports.fetch_reports(current_user.id)
+    past_reports = UserServices::FetchUserReports.fetch_reports(current_user_id)
 
-    accounts = UserServices::FetchUserAccountsSummary.new(current_user.id).call
+    accounts = UserServices::FetchUserAccountsSummary.new(current_user_id).call
 
-    cards = UserServices::FetchUserCardsSummary.new(current_user.id).call
+    cards = UserServices::FetchUserCardsSummary.new(current_user_id).call
 
-    expense_by_category = CategoryServices::FetchExpensesByCategory.call(current_user.id)
-
+    expense_by_category = CategoryServices::FetchExpensesByCategory.call(current_user_id)
     data = report_data.new(
-      UserServices::ConsolidatedUserReport.new(current_user.id).call.decorate,
+      UserServices::ConsolidatedUserReport.new(current_user_id).call.decorate,
       past_reports.decorate,
       UserServices::CreateUserSummaryChartData.call(reports: past_reports),
       accounts,
@@ -27,4 +27,6 @@ class HomeController < ApplicationController
 
     @report_data = data
   end
+
+  delegate :id, to: :current_user, prefix: true
 end
