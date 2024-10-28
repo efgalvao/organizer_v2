bba2a238ed50e0a581d7199d172b88bc4e7c3d76
efgalvao <solfieri@hotmail.com>
2024-10-28 17:56:40 -0300

    Refactor Account model to remove unused code

--- app/models/account/account.rb
+++ app/models/account/account.rb
@@ -7,15 +7,6 @@ module Account
 
     validates :name, presence: true, uniqueness: { scope: :user_id }
 
-    def self.inherited(subclass)
-      super
-      subclass.instance_eval do
-        def model_name
-          Account.model_name
-        end
-      end
-    end
-
     def current_report
       current_report = account_reports.find_by(reference: Time.zone.now.strftime('%m%y'))
       return AccountServices::CreateAccountReport.create_report(id) if current_report.nil?
