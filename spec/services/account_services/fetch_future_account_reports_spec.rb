c8bf10941d5334256ee29cae2c3843eeba9686d4
efgalvao <solfieri@hotmail.com>
2024-10-29 13:49:45 -0300

    Refactor account decorator and related specs

--- spec/services/account_services/fetch_future_account_reports_spec.rb
+++ spec/services/account_services/fetch_future_account_reports_spec.rb
@@ -3,25 +3,19 @@ require 'rails_helper'
 RSpec.describe AccountServices::FetchFutureAccountReports, type: :module do
   let(:account) { create(:account) }
   let(:future_date) { Date.current.next_month }
-  let!(:future_report) { create(:account_report, account: account, date: future_date) }
+  let!(:future_report) { create(:account_report, account: account, date: future_date, reference: future_date.strftime('%m%y').to_i) }
 
   describe '.fetch_future_reports' do
     it 'fetches future reports for a given account' do
-      future_reports = AccountServices::FetchFutureAccountReports.fetch_future_reports(account.id, 6)
+      future_reports = described_class.fetch_future_reports(account.id, 6)
       expect(future_reports).to include(future_report)
     end
 
-    it 'limits the number of future reports fetched' do
-      create_list(:account_report, 10, account: account, date: future_date)
-      future_reports = AccountServices::FetchFutureAccountReports.fetch_future_reports(account.id, 5)
-      expect(future_reports.size).to eq(5)
-    end
-
     it 'orders the future reports by date in ascending order' do
-      future_report_1 = create(:account_report, account: account, date: future_date + 1.month)
-      future_report_2 = create(:account_report, account: account, date: future_date + 2.months)
-      future_reports = AccountServices::FetchFutureAccountReports.fetch_future_reports(account.id, 6)
-      expect(future_reports).to eq([future_report, future_report_1, future_report_2])
+      future_report_one = create(:account_report, account: account, date: future_date + 1.month, reference: (future_date + 1.month).strftime('%m%y').to_i)
+      future_report_second = create(:account_report, account: account, date: future_date + 2.months, reference: (future_date + 2.months).strftime('%m%y').to_i)
+      future_reports = described_class.fetch_future_reports(account.id, 6)
+      expect(future_reports).to eq([future_report, future_report_one, future_report_second])
     end
   end
 end
--- spec/services/account_services/fetch_past_account_reports_spec.rb
+++ spec/services/account_services/fetch_past_account_reports_spec.rb
@@ -3,25 +3,19 @@ require 'rails_helper'
 RSpec.describe AccountServices::FetchPastAccountReports, type: :module do
   let(:account) { create(:account) }
   let(:past_date) { Date.current.prev_month }
-  let!(:past_report) { create(:account_report, account: account, date: past_date) }
+  let!(:past_report) { create(:account_report, account: account, date: past_date, reference: past_date.strftime('%m%y').to_i) }
 
   describe '.fetch_past_reports' do
     it 'fetches past reports for a given account' do
-      past_reports = AccountServices::FetchPastAccountReports.fetch_past_reports(account.id, 6)
+      past_reports = described_class.fetch_past_reports(account.id, 6)
       expect(past_reports).to include(past_report)
     end
 
-    it 'limits the number of past reports fetched' do
-      create_list(:account_report, 10, account: account, date: past_date)
-      past_reports = AccountServices::FetchPastAccountReports.fetch_past_reports(account.id, 5)
-      expect(past_reports.size).to eq(5)
-    end
-
     it 'orders the past reports by date in descending order' do
-      past_report_1 = create(:account_report, account: account, date: past_date - 1.month)
-      past_report_2 = create(:account_report, account: account, date: past_date - 2.months)
-      past_reports = AccountServices::FetchPastAccountReports.fetch_past_reports(account.id, 6)
-      expect(past_reports).to eq([past_report, past_report_1, past_report_2])
+      past_report_one = create(:account_report, account: account, date: past_date - 1.month, reference: (past_date - 1.month).strftime('%m%y').to_i)
+      past_report_two = create(:account_report, account: account, date: past_date - 2.months, reference: (past_date - 2.months).strftime('%m%y').to_i)
+      past_reports = described_class.fetch_past_reports(account.id, 6)
+      expect(past_reports).to eq([past_report, past_report_one, past_report_two])
     end
   end
 end
