2e6a029eb6b6a012876cee102c3a7867bc8e4d4d
efgalvao <solfieri@hotmail.com>
2024-11-12 16:07:01 -0300

    Refactor ideal expense data fetching to use Account::Income model and update related specs

--- spec/services/user_services/fetch_ideal_expense_data_spec.rb
+++ spec/services/user_services/fetch_ideal_expense_data_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe UserServices::FetchIdealExpenseData do
   let(:user) { create(:user) }
   let(:account) { create(:account, user: user) }
-  let(:income_one) { create(:transaction,  amount: 70, account: account, category_id: '11') }
-  let(:income_two) { create(:transaction,  amount: 30, account: account, category_id: '17') }
-  let(:other_user_income) { create(:transaction, amount: 200) }
+  let(:income_one) { create(:income,  amount: 70, account: account, category_id: '11') }
+  let(:income_two) { create(:income,  amount: 30, account: account, category_id: '17') }
+  let(:other_user_income) { create(:income, amount: 200) }
 
   describe '#call' do
     subject(:service) { described_class.new(user.id).call }
 
-    context 'when there are expenses in the group' do
+    context 'when there are incomes in the group' do
       before do
         create(:category, id: 11)
         create(:category, id: 17)
@@ -19,7 +19,7 @@ RSpec.describe UserServices::FetchIdealExpenseData do
         other_user_income
       end
 
-      it 'returns the expenses for the group' do
+      it 'returns the incomes for the group' do
         expect(service).to eq(
           Metas: 15.0,
           Conforto: 15.0,
@@ -32,7 +32,7 @@ RSpec.describe UserServices::FetchIdealExpenseData do
       end
     end
 
-    context 'when there are no expenses in the group' do
+    context 'when there are no incomes in the group' do
       it 'returns an empty array' do
         expect(service).to eq(
           Metas: 0.0,
