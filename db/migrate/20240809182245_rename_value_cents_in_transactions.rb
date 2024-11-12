1a71db58a362e5e28ba1324aa9934f17319f7b0c
efgalvao <solfieri@hotmail.com>
2024-11-12 11:34:25 -0300

    Remove migration to rename value_cents column in transactions and adjust amount type

--- db/migrate/20240809182245_rename_value_cents_in_transactions.rb
+++
@@ -1,7 +0,0 @@
-class RenameValueCentsInTransactions < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :transactions, :value_cents, :amount
-    change_column :transactions, :amount, :decimal, precision: 15, scale: 2
-    add_column :transactions, :type, :string
-  end
-end
