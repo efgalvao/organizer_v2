1239439a27b2b0b15c8e6690956d40e103eb3e99
efgalvao <solfieri@hotmail.com>
2024-11-14 15:13:01 -0300

    Update account retrieval in InvoicesController to filter by account type

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -2,7 +2,7 @@ class InvoicesController < ApplicationController
   before_action :authenticate_user!
 
   def new
-    @accounts = Account::Account.where(user_id: current_user.id).except_card_accounts
+    @accounts = Account::Account.where(type: ['Account::Savings', 'Account::Broker'], user_id: current_user.id)
     @card_id = params[:card_id]
     respond_to do |format|
       format.turbo_stream
