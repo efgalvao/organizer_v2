a9200c9b4d2b5a87a7338dc14cd336436753c1de
efgalvao <solfieri@hotmail.com>
2025-04-28 14:02:17 -0300

    Refactor FetchTransactions service to improve transaction retrieval and add RSpec tests for filtering transactions

--- app/services/transaction_services/fetch_transactions.rb
+++ app/services/transaction_services/fetch_transactions.rb
@@ -12,9 +12,7 @@ module TransactionServices
 
     def call
       fetch_transactions
-    rescue StandardError => e
-      puts "Error fetching transactions: #{e.message}"
-      # error_response(e.message)
+    rescue StandardError
       []
     end
 
@@ -23,24 +21,46 @@ module TransactionServices
     attr_reader :groups, :categories, :user_id
 
     def fetch_transactions
-      Account::Transaction
-        .joins(:account, :category)
-        .includes([:category])
-        .where(account: { user_id: user_id })
-        .where('date >= ? AND date <= ?', Time.zone.today.beginning_of_month, Time.zone.today.end_of_month)
-        .where(group: groups_presents)
-        .where(category: { id: categories })
-        .order(date: :desc)
+      execute_query(
+        fetch_transactions_sql,
+        'fetch_transactions',
+        {
+          user_id: user_id,
+          groups: groups,
+          categories: categories,
+          start_date: Date.today.beginning_of_month,
+          end_date: Date.today.end_of_month
+        }
+      ).to_a
+    end
+
+    def fetch_transactions_sql
+      <<~SQL.squish.strip
+        SELECT *
+        FROM transactions
+        INNER JOIN accounts ON accounts.id = transactions.account_id
+        LEFT JOIN categories ON categories.id = transactions.category_id
+        WHERE accounts.user_id = :user_id
+        AND date >= :start_date AND date <= :end_date
+        #{groups_presents}
+        #{categories_present}
+        ORDER BY date DESC
+      SQL
     end
 
     def groups_presents
-      groups.presence || Account::Transaction.groups.keys
+      groups.present? ? 'AND transactions.group IN (:groups)' : ''
     end
 
     def categories_present
-      return unless categories.present?
+      categories.present? ? 'AND transactions.category_id IN (:categories)' : ''
+    end
 
-      categories
+    def execute_query(query, identifier, params)
+      ApplicationRecord.connection.exec_query(
+        ApplicationRecord.sanitize_sql_array([query, params]),
+        identifier
+      )
     end
   end
 end
---
+++ spec/services/transaction_services/fetch_transactions_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe TransactionServices::FetchTransactions do
+  subject(:fetch_transactions) { described_class.call(params, user.id) }
+
+  let(:account) { create(:account, user: user) }
+  let(:user) { create(:user) }
+
+  before do
+    create(:transaction, account: account, group: 1)
+    create(:transaction, account: account, group: 2)
+    create(:transaction, account: account, group: 0)
+  end
+
+  context 'when the transaction is an income' do
+    let(:params) do
+      {
+        groups: ['0']
+      }
+    end
+
+    it 'fetch filtered transactions', :aggregate_failures do
+      response = fetch_transactions
+
+      expect(response.size).to eq(1)
+    end
+  end
+end
