554051b5c0aa9dfcf5f8d2ceab002713f6156116
efgalvao <solfieri@hotmail.com>
2025-06-12 21:53:31 -0300

    Add UpdateQuote service and corresponding specs
    * Implemented the UpdateQuote service to fetch the latest stock quote and create a position for the specified investment.
    * Added RSpec tests to validate the service's functionality, ensuring correct quote retrieval and position creation.

---
+++ spec/services/investments_services/update_quote_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe InvestmentsServices::UpdateQuote do
+  subject(:service) { described_class.new(investment.id) }
+
+  let(:investment) { create(:investment, name: 'TEST11', shares_total: 10) }
+  let(:quote) { { value: 100.0, date: Date.today.to_s } }
+
+  before do
+    allow(Investments::Investment).to receive(:find).with(investment.id).and_return(investment)
+    allow(InvestmentsServices::FetchStockQuote).to receive(:call).with(investment.name).and_return(quote)
+    allow(InvestmentsServices::CreatePosition).to receive(:call)
+  end
+
+  describe '#call' do
+    it 'fetches the latest quote and creates a position' do
+      service.call
+
+      expect(InvestmentsServices::CreatePosition).to have_received(:call).with(
+        amount: quote[:value],
+        shares: investment.shares_total,
+        date: quote[:date],
+        investment_id: investment.id
+      )
+    end
+  end
+end
