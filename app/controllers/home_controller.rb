833a8ad877494dfa70e4978905c66f452bd86e5a
efgalvao <solfieri@hotmail.com>
2025-08-17 20:22:23 -0300

    Enhance HomeController to include investments by bucket data
    * Updated the HomeController to fetch and include investments categorized by bucket in the report data.
    * Introduced a new service, FetchInvestmentsByBucket, to encapsulate the logic for retrieving investments grouped by their respective buckets.
    * This enhancement improves the investment reporting functionality, providing users with better insights into their investment allocations.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,7 @@ class HomeController < ApplicationController
   def show
     report_data = Struct.new(:user_report, :past_reports, :past_reports_chart_data, :accounts, :cards,
                              :expense_by_category, :expenses_by_group, :ideal_expenses_data,
-                             :investments_allocation_chart_data)
+                             :investments_allocation_chart_data, :investments_by_bucket)
 
     past_reports = UserServices::FetchUserReports.fetch_reports(current_user_id)
 
@@ -24,6 +24,8 @@ class HomeController < ApplicationController
 
     investments_allocation_chart_data = UserServices::FetchInvestmentsAllocation.call(current_user_id)
 
+    investments_by_bucket = InvestmentsServices::FetchInvestmentsByBucket.call(current_user_id)
+
     data = report_data.new(UserServices::ConsolidatedUserReport.new(current_user_id).call.decorate,
                            past_reports.decorate,
                            UserServices::CreateUserSummaryChartData.call(reports: past_reports),
@@ -32,7 +34,8 @@ class HomeController < ApplicationController
                            expense_by_category,
                            expenses_by_group,
                            ideal_expenses_data,
-                           investments_allocation_chart_data)
+                           investments_allocation_chart_data,
+                           investments_by_bucket)
 
     @report_data = data
   end
