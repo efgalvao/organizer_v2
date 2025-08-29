ebc55fac974405eb50f030cd021532f1ddf553fe
efgalvao <solfieri@hotmail.com>
2025-08-28 21:35:48 -0300

    Refactor boot.rb to improve debug gem loading
    * Updated boot.rb to conditionally require the debug gem within a begin-rescue block, enhancing readability and ensuring that the gem is only loaded in development and test environments. This change improves error handling by adding a comment for production context.

--- config/boot.rb
+++ config/boot.rb
@@ -3,7 +3,10 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
 
-begin
-  require "debug" if %w[development test].include?(ENV["RAILS_ENV"])
-rescue LoadError
+if %w[development test].include?(ENV["RAILS_ENV"])
+  begin
+    require "debug"
+  rescue LoadError
+    # ignora em produção
+  end
 end
