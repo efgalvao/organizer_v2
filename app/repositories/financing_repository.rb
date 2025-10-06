1998f63ff5e8bb575f3003b9f35772df355608ed
efgalvao <solfieri@hotmail.com>
2025-10-05 21:34:25 -0300

    Add FinancingRepository and corresponding tests for financing management
    * Implemented FinancingRepository to handle CRUD operations for financings.
    * Created RSpec tests to ensure functionality of all methods in FinancingRepository, including retrieval, creation, updating, and deletion of financing records.

---
+++ app/repositories/financing_repository.rb
@@ -0,0 +1,22 @@
+class FinancingRepository
+  def all(user_id)
+    Financings::Financing.where(user_id: user_id).all
+  end
+
+  def create!(attributes)
+    Financings::Financing.create!(attributes)
+  end
+
+  def update!(financing, attributes)
+    financing.update!(attributes)
+    financing
+  end
+
+  def find_by(attributes = {})
+    Financings::Financing.find_by(attributes)
+  end
+
+  def destroy(id)
+    Financings::Financing.delete(id)
+  end
+end
