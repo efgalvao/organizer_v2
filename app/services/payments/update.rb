f0791c5468db2f2b133fe30aaffceed9d1fcab9f
efgalvao <solfieri@hotmail.com>
2026-03-05 21:03:10 -0300

    Add Payments::Create and Payments::Update services with RSpec tests
    * Introduced Payments::Create service to handle payment creation logic, including parameter validation and error handling.
    * Introduced Payments::Update service to manage payment updates, ensuring existing payments can be modified correctly.
    * Added RSpec tests for both services to verify functionality and correctness, covering various scenarios for payment creation and updates.
    * Enhanced code organization and maintainability within the Payments module.

---
+++ app/services/payments/update.rb
@@ -0,0 +1,51 @@
+module Payments
+  class Update
+    def initialize(payment_id, params)
+      @payment_id = payment_id
+      @params = params
+      @payment_repository = PaymentRepository
+    end
+
+    def self.call(payment_id, params)
+      new(payment_id, params).call
+    end
+
+    def call
+      update_payment
+    end
+
+    private
+
+    attr_reader :payment_id, :params, :payment_repository
+
+    def payment
+      payment_repository.find_by({ id: payment_id })
+    end
+
+    def update_payment
+      payment_repository.update!(payment, update_params)
+      payment
+    end
+
+    def update_params
+      {
+        parcel: params[:parcel],
+        paid_parcels: params[:paid_parcels],
+        ordinary: params[:ordinary],
+        payment_date: params[:payment_date],
+        amortization: value_to_decimal(params[:amortization]),
+        interest: value_to_decimal(params[:interest]),
+        fees: value_to_decimal(params[:fees]),
+        insurance: value_to_decimal(params[:insurance]),
+        adjustment: value_to_decimal(params[:adjustment]),
+        monetary_correction: value_to_decimal(params[:monetary_correction])
+      }
+    end
+
+    def value_to_decimal(value)
+      return 0 if value.nil?
+
+      value.to_d.round(2)
+    end
+  end
+end
