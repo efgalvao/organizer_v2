6b6f6b1dedd942f6f99ca64d38ec9097c41a804a
efgalvao <solfieri@hotmail.com>
2026-07-22 17:05:58 -0300

    Refactor asset_class_target_spec to remove explicit type declaration
    - Simplified the RSpec describe block for AssetClassTarget by removing the type declaration, enhancing readability and maintaining consistency with other specs.

--- spec/models/asset_class_target_spec.rb
+++ spec/models/asset_class_target_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe AssetClassTarget, type: :model do
+RSpec.describe AssetClassTarget do
   describe 'associations' do
     it { is_expected.to belong_to(:user) }
   end
